55552aa6ea380acfa3b3ce61adf41c2d33bafa8e
hima <himasri0624@gmail.com>
2026-02-04 00:06:30 +0530

    010226-new inplementations

---
+++ EMS1.API/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EMS1.API.Models;
+using EMS1.API.Data;
+
+namespace EMS1.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            
+            return Ok(_context.Employees.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            _context.Employees.Add(emp);
+            _context.SaveChanges();
+            return Ok(emp);
+        }
+    }
+}
---
+++ EMS1.API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EMS1.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS1.API.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
